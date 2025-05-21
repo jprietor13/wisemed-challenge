b5ac1755a89966073dbbd58fe08018465a450952
Juan Prieto <jp1739@gmail.com>
2025-05-21 09:16:34 -0500

    feat: add homepage

--- src/routes/RouteApp.tsx
+++ src/routes/RouteApp.tsx
@@ -4,7 +4,7 @@ import { Layout } from "../components/layout/Layout";
 
 export const RouteApp = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/patient-card">
       <Routes>
         <Route index element={<Login />} />
         <Route path="/home" element={<Layout />} />
